b425a4f0a6f77e5d276607158d74eafb6d9ec544
shreyas1803 <134675692+shreyas1803@users.noreply.github.com>
2023-06-03 18:41:39 +0530

    Update bresenham_line.cpp

--- bresenham_line.cpp
+++ bresenham_line.cpp
@@ -2,86 +2,95 @@
 #include <GL/gl.h>
 #include <cstdlib>
 
-void display(int x, int y){
-	glColor3f(0, 1, 0);
+void setpixel(int x, int y){
+	glColor3f(1,0 , 0);
 	glBegin(GL_POINTS);
 	glVertex2i(x,y);
 	glEnd();
+	glFlush();
+	
 }
 
 void DrawLine(int x1, int y1, int x2, int y2)
 {
-	int dy = y2 - y1;
-	int dx = x2 - x1;
-	int xc, yc;
-	int x = x1;
-	int y = y1;
-	
-	if(x1>x2){
-		xc = -1;
-	}else{
-		xc = 1;
-	}
-	if(y1>y2){
-		yc = -1;
-	}else{
-		yc = 1;
-	}
-	
-	if(dx == 0){
-		for(int i=0;i<dy;i++){
-			y+=yc;
-			display(x, y);
-		}
-	}
-	if(dy == 0){
-		for(int i=0;i<dx;i++){
-			x+=xc;
-			display(x, y);
-		}
-	}
-	if(dx>dy)
-	{
-		int p = (2*dy)-dx;
-		for(int i=0;i<dx;i++){
-			if(p>0){
-				y = y+yc;
-				p = p + (2*dy-2*dx);
-			}
-			else{
-				p = p + (2*dy);
-			}
-			x = x + xc;
-			display(x, y);
-		}
-	}
-	if(dy>dx)
-	{
-		int p = (2*dx)-dy;
-		for(int i=0;i<dy;i++){
-			if(p>0){
-				x = x+xc;
-				p = p + (2*dx-2*dy);
-			}
-			else{
-				p = p + (2*dx);
-			}
-			y = y + yc;
-			display(x, y);
-		}
-	}
+    float dy = (y2 - y1);
+    float dx = (x2 - x1);
+
+    if (dx < 0) {
+        dx = -dx;
+    }
+    if (dy < 0) {
+        dy = -dy;
+    }
+    float xin = 1;
+    if (x2 < x1) {
+        xin = -1;
+    }
+    float yin = 1;
+    if (y2 < y1) {
+        yin = -1;
+    }
+
+    float x = x1;
+    float y = y1;
+
+    if (dx > dy) {
+        setpixel(x, y);
+
+        float p = 2 * dy - dx;
+        float inc1 = 2 * (dy - dx);
+        float inc2 = 2 * dy;
+
+        for (int i = 0;i < dx;i++) {
+
+            if (p >= 0) {
+                y += yin;
+                p += inc1;
+
+            }
+            else {
+
+                x += xin;
+                p += inc2;
+
+            }
+            setpixel(x, y);
+        }
+
+    }
+    else {
+        setpixel(x, y);
+        float p = 2 * dx - dy;
+        float inc1 = 2 * (dx - dy);
+        float inc2 = 2 * dx;
+
+        for (int i = 0;i < dy;i++) {
+            if (p >= 0) {
+                p += inc1;
+                x += xin;
+            }
+            else {
+                p += inc1;
+                y += yin;
+            }
+            setpixel(x, y);
+        }
+    }
+    glFlush();
 }
 
 void renderFunction()
 {
-    glClearColor(1,0,0,1);
+    glClearColor(0,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1,0,0);
+    glColor3f(0,0,0);
     // glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     gluOrtho2D(0, 700, 0, 700);
-	DrawLine(100, 300, 170, 500);
-	DrawLine(171, 501, 300, 300);
-	DrawLine(300, 300, 100, 300);
+	DrawLine(100, 100, 100, 150);
+	DrawLine(100, 150, 200, 150);
+	DrawLine(200, 150, 200, 100);
+	DrawLine(200, 100, 100, 100);			
+
     glFlush();
 }
 
@@ -89,7 +98,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(700,700);
+    glutInitWindowSize(1400,900);
     glutInitWindowPosition(100,100);
     glutCreateWindow("OpenGL - First window demo");
     glutDisplayFunc(renderFunction);
